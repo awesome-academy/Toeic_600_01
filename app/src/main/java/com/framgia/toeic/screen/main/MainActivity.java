351e133b8da2815079d9ea483a8c8a66723028da
thanh <thanh01684265170@gmail.com>
2019-02-13 09:13:30 +0700

    ref task 6086: edit home screen

--- app/src/main/java/com/framgia/toeic/screen/main/MainActivity.java
+++ app/src/main/java/com/framgia/toeic/screen/main/MainActivity.java
@@ -2,19 +2,18 @@ package com.framgia.toeic.screen.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-import android.widget.ViewFlipper;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.framgia.toeic.R;
 import com.framgia.toeic.data.model.Mark;
@@ -30,27 +29,13 @@ import com.framgia.toeic.screen.exam.ExamActivity;
 import com.framgia.toeic.screen.grammar.GrammarActivity;
 import com.framgia.toeic.screen.vocabulary.VocabularyActivity;
 
-import java.util.List;
-
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener,
-        View.OnClickListener, MainContract.View {
-    private static final int ID_VOCABULARY = 0;
-    private static final int ID_GRAMMAR = 1;
-    private static final int ID_BASIC_TEST = 2;
-    private static final int ID_EXAM = 3;
-    private static final int BACKGROUNDS_INTRO[] = {R.drawable.bg_intro_1,
-            R.drawable.bg_intro_2, R.drawable.bg_intro_3, R.drawable.bg_intro_4};
+        View.OnClickListener {
     private Toolbar mToolbar;
     private NavigationView mNavigationView;
-    private ViewFlipper mViewFlipper;
-    private ProgressBar mProgressVocabulary;
-    private ProgressBar mProgressGrammar;
-    private ProgressBar mProgressBasicTest;
-    private ProgressBar mProgressExam;
     private DrawerLayout mDrawer;
-    private Button mButtonVocabulary, mButtonGrammar, mButtonBasicTest, mButtonExam;
-    private MainContract.Presenter mPresenter;
+    private CardView mCardVocabulary, mCardGrammar, mCardBasicTest, mCardExam, mCardUser;
 
     public static Intent getMainIntent(Context context) {
         return new Intent(context, MainActivity.class);
@@ -58,7 +43,18 @@ public class MainActivity extends BaseActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Window window = getWindow();
         super.onCreate(savedInstanceState);
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.marterial_indigo_500));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -77,41 +73,23 @@ public class MainActivity extends BaseActivity
         toggle.syncState();
         mNavigationView = findViewById(R.id.nav_view);
         mNavigationView.setNavigationItemSelectedListener(this);
-        mViewFlipper = findViewById(R.id.viewflipper);
-        mProgressVocabulary = findViewById(R.id.progress_vocabulary);
-        mProgressGrammar = findViewById(R.id.progress_grammar);
-        mProgressBasicTest = findViewById(R.id.progress_basic_test);
-        mProgressExam = findViewById(R.id.progress_exam);
-        mButtonVocabulary = findViewById(R.id.button_vocabulary);
-        mButtonGrammar = findViewById(R.id.button_grammar);
-        mButtonBasicTest = findViewById(R.id.button_basic_test);
-        mButtonExam = findViewById(R.id.button_exam);
-        mPresenter = new MainPresenter(MarkRepository.getInstance(
-                new MarkLocalDataSource(new MarkDatabaseHelper(new DBHelper(this)))),
-                VocabularyLessonRepository.getInstance(new VocabularyLessonLocalDataSource(
-                        new VocabularyLessonDatabaseHelper(new DBHelper(this)))), this);
+        mNavigationView.setBackgroundColor(getResources().getColor(R.color.marterial_deep_purple_200));
+        mCardVocabulary = findViewById(R.id.card_vocabulary);
+        mCardBasicTest = findViewById(R.id.card_basic_test);
+        mCardGrammar = findViewById(R.id.card_grammar);
+        mCardExam = findViewById(R.id.card_exam);
+        mCardUser = findViewById(R.id.card_user);
     }
 
     @Override
     protected void initData() {
-        slideAdvertise();
-        mProgressVocabulary.getProgressDrawable().setColorFilter(
-                getResources().getColor(R.color.material_orange_300),
-                android.graphics.PorterDuff.Mode.MULTIPLY);
-        mProgressGrammar.getProgressDrawable().setColorFilter(
-                getResources().getColor(R.color.material_brown_300),
-                android.graphics.PorterDuff.Mode.MULTIPLY);
-        mProgressBasicTest.getProgressDrawable().setColorFilter(
-                getResources().getColor(R.color.material_blue_accent_400),
-                android.graphics.PorterDuff.Mode.MULTIPLY);
-        mProgressExam.getProgressDrawable().setColorFilter(
-                getResources().getColor(R.color.material_red_accent_200),
-                android.graphics.PorterDuff.Mode.MULTIPLY);
-        mButtonVocabulary.setOnClickListener(this);
-        mButtonGrammar.setOnClickListener(this);
-        mButtonBasicTest.setOnClickListener(this);
-        mButtonExam.setOnClickListener(this);
-        mPresenter.getNumberQuestion();
+        getSupportActionBar().setBackgroundDrawable(new
+                ColorDrawable(getResources().getColor(R.color.marterial_indigo_400)));
+        mCardVocabulary.setOnClickListener(this);
+        mCardGrammar.setOnClickListener(this);
+        mCardBasicTest.setOnClickListener(this);
+        mCardExam.setOnClickListener(this);
+        mCardUser.setOnClickListener(this);
     }
 
     @Override
@@ -163,59 +141,27 @@ public class MainActivity extends BaseActivity
     @Override
     protected void onStart() {
         super.onStart();
-        mPresenter.getMarks();
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button_vocabulary:
+            case R.id.card_vocabulary:
                 startActivity(VocabularyActivity.getVocabularyIntent(this));
                 break;
 
-            case R.id.button_grammar:
+            case R.id.card_grammar:
                 startActivity(GrammarActivity.getGrammarIntent(this));
                 break;
 
-            case R.id.button_basic_test:
+            case R.id.card_basic_test:
                 break;
 
-            case R.id.button_exam:
+            case R.id.card_exam:
                 startActivity(ExamActivity.getExamActivity(this));
                 break;
+            case R.id.card_user:
+                break;
         }
-    }
-
-    private void slideAdvertise() {
-        for (int image : BACKGROUNDS_INTRO) {
-            flipperImage(image);
-        }
-    }
-
-    private void flipperImage(int image) {
-        ImageView imageView = new ImageView(this);
-        imageView.setBackgroundResource(image);
-        mViewFlipper.addView(imageView);
-        mViewFlipper.setAutoStart(true);
-        mViewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
-        mViewFlipper.setOutAnimation(this, android.R.anim.slide_out_right);
-    }
-
-    @Override
-    public void showValueProgressBar(List<Mark> marks) {
-        mProgressVocabulary.setProgress(marks.get(ID_VOCABULARY).getMark());
-        mProgressGrammar.setProgress(marks.get(ID_GRAMMAR).getMark());
-        mProgressBasicTest.setProgress(marks.get(ID_BASIC_TEST).getMark());
-        mProgressExam.setProgress(marks.get(ID_EXAM).getMark());
-    }
-
-    @Override
-    public void setMaxSizeProgressBars(List<Integer> integers) {
-        mProgressVocabulary.setMax(integers.get(ID_VOCABULARY));
-    }
-
-    @Override
-    public void showError(Exception error) {
-        Toast.makeText(this, error.getMessage(), Toast.LENGTH_LONG).show();
     }
 }
